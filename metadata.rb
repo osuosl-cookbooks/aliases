2a79fe95ab8718058224ed23f2a6b2db8d604c02
Trevor Bramwell <bramwelt@osuosl.org>
2013-10-18 17:02:33 -0700

    Put project under Apache 2.0
    Open source is how we roll.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'aliases'
 maintainer       'OSU Open Source Lab'
 maintainer_email 'rudy@grigar.net'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures aliases'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
